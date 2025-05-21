ce16574ff18d39e9ef44a2f712b3fc8b2715c1e4
Troth99 <trothlol123456@gmail.com>
2025-05-21 19:26:15 +0300

    updated 2 tasks

--- Advanced Data Types-Exercise/operator.ts
+++ Advanced Data Types-Exercise/operator.ts
@@ -2,22 +2,23 @@ function operator(
   params: string | number | string[],
   operations: string,
   operand: number
-): void {
+) {
   let result;
-  if (operations === "Index" && (typeof params === "string" || Array.isArray(params))) {
-      result = params[operand];
-  
-  } else if (operations === "Length" && (typeof params === "string" || Array.isArray(params))) {
-    
+  if (
+    operations === "Index" &&
+    (typeof params === "string" || Array.isArray(params))
+  ) {
+    result = params[operand];
+  } else if (
+    operations === "Length" && (typeof params !== "number")
+  ) {
     result = params.length % operand;
-
-  } else if (operations === "Add") {
-    if (typeof params === "string" || typeof params === 'number') {
+  } else if (operations === "Add" &&  (typeof params === "string" || typeof params === "number")) {
       result = Number(params) + operand;
-    }
+  
   }
 
-  console.log(result);
+  return result;
 }
 
-operator(['Just', 'Two'], 'Length', 5);
+console.log(operator(["Just", "Two"], "Length", 5));
--- Advanced Data Types-Exercise/out/operator.js
+++ Advanced Data Types-Exercise/out/operator.js
@@ -1,18 +1,17 @@
 "use strict";
 function operator(params, operations, operand) {
     let result;
-    if (operations === "Index" && (typeof params === "string" || Array.isArray(params))) {
+    if (operations === "Index" &&
+        (typeof params === "string" || Array.isArray(params))) {
         result = params[operand];
     }
-    else if (operations === "Length" && (typeof params === "string" || Array.isArray(params))) {
+    else if (operations === "Length" && (typeof params !== "number")) {
         result = params.length % operand;
     }
-    else if (operations === "Add") {
-        if (typeof params === "string" || typeof params === 'number') {
-            result = Number(params) + operand;
-        }
+    else if (operations === "Add" && (typeof params === "string" || typeof params === "number")) {
+        result = Number(params) + operand;
     }
-    console.log(result);
+    return result;
 }
-operator(['Just', 'Two'], 'Length', 5);
+console.log(operator(["Just", "Two"], "Length", 5));
 //# sourceMappingURL=operator.js.map
